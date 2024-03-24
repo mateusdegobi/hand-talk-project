14f4af1d27191d3591020fcc3e5b3abe6a9cd940
mateus degobi <kim3dis@gmail.com>
2024-03-24 01:59:43 -0300

    chore: add firebase configs

---
+++ src/constants/database/firebase.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_APIKEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECTID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.EXPO_PUBLIC_APPID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENTID,
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
+};
+
+export default firebaseConfig;

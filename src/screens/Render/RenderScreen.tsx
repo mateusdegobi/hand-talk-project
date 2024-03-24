b6e1c9a1686b9e446abfd51758306dfdc4181b6f
mateus degobi <kim3dis@gmail.com>
2024-03-24 02:41:02 -0300

    feat: adds exit button and settings button to render screen

--- src/screens/Render/RenderScreen.tsx
+++ src/screens/Render/RenderScreen.tsx
@@ -1,11 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
+import Button from '@src/components/Button/Button';
+import Container from '@src/components/Container/Container';
 import { Box, Cone, Dodecahedron } from '@src/components/GeometricFigures';
+import useAuth from '@src/hooks/useAuth';
 import React from 'react';
 
-import { CanvasView, Container } from './styles';
+import { CanvasView, Header } from './styles';
 
 export default function RenderScreen() {
+  const { onSignOut } = useAuth();
+  const { navigate } = useNavigation();
+
+  const goToSettings = () => {
+    navigate('Settings');
+  };
+
   return (
-    <Container>
+    <Container style={{ backgroundColor: 'black' }}>
+      <Header>
+        <Button type="tertiary" onPress={onSignOut}>
+          Sair
+        </Button>
+      </Header>
+
       <CanvasView>
         <ambientLight intensity={1} />
         <directionalLight position={[10, 10, 10]} intensity={1.5} />
@@ -14,6 +31,10 @@ export default function RenderScreen() {
         <Cone color="yellow" position={[0, 0, 0]} />
         <Dodecahedron color="green" position={[0, -1.5, 0]} />
       </CanvasView>
+
+      <Button type="primary" onPress={goToSettings}>
+        Configurações
+      </Button>
     </Container>
   );
 }
--- src/screens/Render/styles.ts
+++ src/screens/Render/styles.ts
@@ -1,12 +1,12 @@
 import { Canvas } from '@react-three/fiber';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  flex: 1;
-  background-color: #000;
-`;
-
 export const CanvasView = styled(Canvas)`
   flex: 1;
   width: 100%;
+`;
+
+export const Header = styled.View`
+  align-items: flex-end;
+  margin: 10px 0;
 `;

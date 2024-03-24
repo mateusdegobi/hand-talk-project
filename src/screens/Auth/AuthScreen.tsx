f6e0202d6867bf10f85245dc1c985e9776aa21e5
mateus degobi <kim3dis@gmail.com>
2024-03-24 02:39:09 -0300

    feat: configure inputs

--- src/screens/Auth/AuthScreen.tsx
+++ src/screens/Auth/AuthScreen.tsx
@@ -1,7 +1,10 @@
 import HandTalkLogoImage from '@assets/handtalk-logo/handtalk-logo.png';
+import { auth } from '@src/auth/firebase_auth';
 import Container from '@src/components/Container/Container';
 import TextInputWithLabel from '@src/components/Input/InputWithLabel/InputWithLabel';
-import React, { useCallback, useRef } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { Button, TextInput } from 'react-native';
 
 import { HandTalkLogo } from './styles';
@@ -12,26 +15,61 @@ export default function AuthScreen() {
 
   const focusPassword = () => passwordRef.current?.focus();
 
-  const onSubmit = useCallback(() => {}, []);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async () => {
+    await handleSubmit(async ({ email, password }) => {
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        console.log(errors);
+      }
+    })();
+  };
 
   return (
     <Container>
       <HandTalkLogo source={HandTalkLogoImage} />
 
-      <TextInputWithLabel
-        textLabel="Email"
-        ref={inputEmailRef}
-        placeholder="handtalk@mail.com"
-        returnKeyType="next"
-        onSubmitEditing={focusPassword}
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { value, onChange } }) => (
+          <TextInputWithLabel
+            ref={inputEmailRef}
+            textLabel="Email"
+            placeholder="handtalk@mail.com"
+            returnKeyType="next"
+            onSubmitEditing={focusPassword}
+            value={value}
+            onChangeText={onChange}
+          />
+        )}
       />
-      <TextInputWithLabel
-        textLabel="Senha"
-        ref={passwordRef}
-        placeholder="********"
-        returnKeyType="go"
-        secureTextEntry
-        onSubmitEditing={onSubmit}
+      <Controller
+        control={control}
+        name="password"
+        render={({ field: { value, onChange } }) => (
+          <TextInputWithLabel
+            textLabel="Senha"
+            ref={passwordRef}
+            placeholder="********"
+            returnKeyType="go"
+            secureTextEntry
+            onSubmitEditing={onSubmit}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
       />
 
       <Button title="Entrar" color="orange" onPress={onSubmit} />

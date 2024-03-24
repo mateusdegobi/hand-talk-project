532827df09a125adf60694c14773264098a31c7e
mateus degobi <kim3dis@gmail.com>
2024-03-24 01:58:49 -0300

    chore: configure auth on routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,12 @@
+import useAuth from '@src/hooks/useAuth';
 import React from 'react';
 
 import AuthenticatedNavigator from './Authenticated';
 import PublicNavigator from './Public';
 
 function Routes() {
-  const auth = false;
-
-  return auth ? <AuthenticatedNavigator /> : <PublicNavigator />;
+  const { isAuth } = useAuth();
+  return isAuth ? <AuthenticatedNavigator /> : <PublicNavigator />;
 }
 
 export default Routes;

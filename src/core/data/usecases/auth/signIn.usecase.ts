7f1ec696ff7c67a071def729f23e88e81245b73f
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:31:12 -0300

    refactor: add verify credentials

--- src/core/data/usecases/auth/signIn.usecase.ts
+++ src/core/data/usecases/auth/signIn.usecase.ts
@@ -4,6 +4,14 @@ export class SignIn {
   constructor(private readonly repo: AuthContract) {}
 
   async execute(email: string, password: string) {
+    if (!email && email.length < 5) {
+      throw new Error('Email is required');
+    }
+
+    if (!password) {
+      throw new Error('Password is required');
+    }
+
     return this.repo.signIn(email, password);
   }
 }

ea1e9f32bba0be2facb9615506339e8699c2dfdb
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:29:56 -0300

    refactor: change types

--- src/core/data/contracts/user-objects-config.contract.ts
+++ src/core/data/contracts/user-objects-config.contract.ts
@@ -1,8 +1,8 @@
-import { GeometricObject } from '@src/core/domain/entities/GeometricObject';
+import { GeometricObject, ObjectsType } from '@src/core/domain/entities/GeometricObject';
 
 export type GeometricObjectUserData = Omit<GeometricObject, 'position'>;
 
 export interface UserConfigObjectContract {
   getConfig(userKey: string): Promise<GeometricObjectUserData | undefined>;
-  setConfig(userKey: string, config: GeometricObjectUserData): Promise<void>;
+  setConfig(userKey: string, config: ObjectsType): Promise<void>;
 }

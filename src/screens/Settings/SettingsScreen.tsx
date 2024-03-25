817851957c9ece71468edbf39209bcfe9541e293
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:33:56 -0300

    feat: add submit settings

--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -3,49 +3,72 @@ import Button from '@src/components/Button/Button';
 import Container from '@src/components/Container/Container';
 import { useObjectsContext } from '@src/contexts/ObjectsContext';
 import { ObjectsType } from '@src/core/domain/entities/GeometricObject';
-import React, { useState } from 'react';
+import { ClockClockwise, FloppyDisk } from 'phosphor-react-native';
+import React, { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
 
-import { ScrollView } from './styles';
-import { ObjectKey } from './types';
 import GeometricObjectSettings from './components/EditableGeometricFigure';
+import { ScrollView } from './styles';
 
 export default function SettingsScreen() {
   const { goBack } = useNavigation();
 
-  const { objects, onEditObject } = useObjectsContext();
+  const { objects, onEditAllObjects, setConfigObjects } = useObjectsContext();
 
-  const [editObject, setEditObject] = useState<ObjectsType | undefined>(objects);
+  const { control, handleSubmit } = useForm();
 
-  const handleSave = async () => {
-    await onEditObject('object1', editObject?.object1);
-    await onEditObject('object2', editObject?.object2);
-    await onEditObject('object3', editObject?.object3);
-    goBack();
-  };
+  const handleSave = useCallback(async () => {
+    await handleSubmit(async (data) => {
+      const objectsToUpdate: ObjectsType = {
+        object1: { ...objects?.object1 },
+        object2: { ...objects?.object2 },
+        object3: { ...objects?.object3 },
+      };
 
-  const handleEditObject = (
-    objectKey: ObjectKey,
-    object: ObjectsType[ObjectKey],
-    color: string
-  ) => {
-    setEditObject({
-      ...editObject,
-      [objectKey]: { ...object, color },
-    });
-  };
+      Object.entries(data).forEach(([key, value]) => {
+        const [objectId, property] = key.split('-');
+        if (property === 'rotation') {
+          value = value ? parseInt(value, 10) : 0;
+        }
+        if (value !== undefined) {
+          objectsToUpdate[objectId] = {
+            ...objectsToUpdate[objectId],
+            [property]: value,
+          };
+        }
+      });
+
+      try {
+        await onEditAllObjects(objectsToUpdate);
+        setConfigObjects(objectsToUpdate);
+      } catch (e) {
+        console.log(e);
+      }
+
+      goBack();
+    })();
+  }, [
+    goBack,
+    handleSubmit,
+    objects?.object1,
+    objects?.object2,
+    objects?.object3,
+    onEditAllObjects,
+    setConfigObjects,
+  ]);
 
   return (
     <Container>
       <ScrollView showsVerticalScrollIndicator={false}>
         {Object.entries(objects || {}).map(([name, object]) => (
-          <GeometricObjectSettings name={name} object={object} editObject={handleEditObject} />
+          <GeometricObjectSettings name={name} control={control} object={object} />
         ))}
       </ScrollView>
 
-      <Button type="tertiary" onPress={goBack}>
+      <Button type="tertiary" onPress={goBack} icon={ClockClockwise}>
         Restaurar para padrão
       </Button>
-      <Button type="primary" onPress={handleSave} style={{ marginTop: 10 }}>
+      <Button type="primary" onPress={handleSave} icon={FloppyDisk} style={{ marginTop: 10 }}>
         Salvar
       </Button>
     </Container>

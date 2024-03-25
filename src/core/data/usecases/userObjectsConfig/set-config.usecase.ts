be150dcd8648f37dd96873497bd74743aa59e762
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:31:37 -0300

    refactor: change types

--- src/core/data/usecases/userObjectsConfig/set-config.usecase.ts
+++ src/core/data/usecases/userObjectsConfig/set-config.usecase.ts
@@ -1,9 +1,11 @@
-import { GeometricObjectUserData, UserConfigObjectContract } from '../../contracts';
+import { ObjectsType } from '@src/core/domain/entities/GeometricObject';
+
+import { UserConfigObjectContract } from '../../contracts';
 
 export class SetUserObjectsConfig {
   constructor(private readonly repo: UserConfigObjectContract) {}
 
-  async execute(userKey: string, config: GeometricObjectUserData) {
+  async execute(userKey: string, config: ObjectsType) {
     await this.repo.setConfig(userKey, config);
   }
 }

773476f60ae1008872e6b0660668af8ecb7beb47
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:22:07 -0300

    refactor: add function to send all objects together

--- src/contexts/ObjectsContext/index.tsx
+++ src/contexts/ObjectsContext/index.tsx
@@ -1,18 +1,23 @@
-import { GeometricObject, ObjectsType } from '@src/core/domain/entities/GeometricObject';
+import { GeometricObjectUserData } from '@src/core/data/contracts';
+import { ObjectsType, PartialGeometricObject } from '@src/core/domain/entities/GeometricObject';
 import { useObjects, useAuth, useUserObjectsConfig } from '@src/hooks/';
-import React, { createContext, useCallback, useContext, useEffect, useMemo } from 'react';
+import { createContext, useCallback, useContext, useEffect, useMemo, ReactNode } from 'react';
 
 type ObjectsContextType = {
   objects?: ObjectsType;
-  onEditObject: (keyObject: string, data: Partial<GeometricObject>) => Promise<void>;
+  onEditObject: (keyObject: string, data: PartialGeometricObject | undefined) => Promise<void>;
+  onEditAllObjects: (newObjects: ObjectsType) => Promise<void>;
+  setConfigObjects: (newObjects: GeometricObjectUserData) => Promise<void>;
 };
 export const ObjectsContext = createContext<ObjectsContextType>({
   objects: undefined,
   onEditObject: async () => {},
+  onEditAllObjects: async () => {},
+  setConfigObjects: async () => {},
 });
 
-function ObjectsProvider({ children }: { children: React.ReactNode }) {
-  const { objects, onEditObject } = useObjects();
+function ObjectsProvider({ children }: { children: ReactNode }) {
+  const { objects, onEditObject, onEditAllObjects } = useObjects();
   const { getConfig, setConfig } = useUserObjectsConfig();
   const { user } = useAuth();
 
@@ -21,24 +26,35 @@ function ObjectsProvider({ children }: { children: React.ReactNode }) {
 
     if (userUid) {
       const userObjects = await getConfig(userUid);
-
       if (userObjects) {
-        for (const [key, value] of Object.entries(userObjects)) {
-          await onEditObject(key, value);
-        }
+        await onEditAllObjects(userObjects);
       }
     }
-  }, [getConfig, onEditObject, user?.uid]);
+  }, [getConfig, onEditAllObjects, user?.uid]);
+
   useEffect(() => {
     mergeObjects();
   }, [mergeObjects]);
+
+  const setConfigObjects = useCallback(
+    async (newObjects: ObjectsType) => {
+      const userUid = user?.uid;
+
+      if (userUid) {
+        await setConfig(userUid, newObjects);
+      }
+    },
+    [setConfig, user?.uid]
+  );
 
   const value = useMemo(
     () => ({
       objects,
       onEditObject,
+      onEditAllObjects,
+      setConfigObjects,
     }),
-    [objects, onEditObject]
+    [objects, onEditObject, onEditAllObjects, setConfigObjects]
   );
 
   return <ObjectsContext.Provider value={value}>{children}</ObjectsContext.Provider>;

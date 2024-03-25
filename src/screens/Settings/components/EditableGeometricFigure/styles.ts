5f18c0c5d3572fe0e497fe771a8819640f31999a
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:16:23 -0300

    style: adjust row

--- src/screens/Settings/components/EditableGeometricFigure/styles.ts
+++ src/screens/Settings/components/EditableGeometricFigure/styles.ts
@@ -2,8 +2,24 @@ import styled from 'styled-components/native';
 
 export const Row = styled.View`
   flex-direction: row;
+  align-items: center;
+  padding: 0 10px;
+`;
+
+export const CanvasArea = styled.View`
+  align-items: center;
+  justify-content: center;
+  width: 120px;
+  height: 120px;
 `;
 
 export const InputArea = styled.View`
   flex: 1;
+`;
+
+export const Container = styled.View`
+  background-color: #f4f4f4;
+  border-radius: 12px;
+  margin: 10px 0;
+  padding: 10px 0;
 `;

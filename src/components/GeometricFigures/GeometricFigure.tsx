ae08bb0928d0b03542292c322da5431d9edcc5b7
mateus degobi <kim3dis@gmail.com>
2024-03-24 22:18:34 -0300

    fix: add rotation and scale support

--- src/components/GeometricFigures/GeometricFigure.tsx
+++ src/components/GeometricFigures/GeometricFigure.tsx
@@ -1,23 +1,37 @@
 import { MeshProps } from '@react-three/fiber';
 import { ShapeType } from '@src/core/domain/entities/GeometricObject';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { BufferGeometry, Material, Mesh } from 'three';
 
-import { Cone } from './Cone';
-import { Cube } from './Cube';
+const renderShape = {
+  cube: <boxGeometry args={[0.7, 0.7, 0.7]} />,
+  cone: <coneGeometry args={[0.5, 0.7, 32]} />,
+  dodecahedron: <dodecahedronGeometry args={[0.5, 0]} />,
+};
 
 type GeometricFigureProps = {
   color: string;
   shape: ShapeType;
+  scale: number;
+  rotation: number;
 } & MeshProps;
-export function GeometricFigure({ shape, color, ...rest }: GeometricFigureProps) {
-  if (shape === 'cone') {
-    return <Cone color={color} {...rest} />;
-  }
+export function GeometricFigure({ rotation, scale, shape, color, ...rest }: GeometricFigureProps) {
+  const mesh = useRef<Mesh<BufferGeometry, Material | Material[]>>(null);
 
-  if (shape === 'cube') {
-    return <Cube color={color} {...rest} />;
-  }
+  const changeMeshRotation = useCallback(() => {
+    if (mesh.current) {
+      mesh.current.rotation.z = rotation || 0;
+    }
+  }, [rotation]);
 
-  if (shape === 'dodecahedron') {
-    return <Cone color={color} {...rest} />;
-  }
+  useEffect(() => {
+    changeMeshRotation();
+  }, [changeMeshRotation]);
+
+  return (
+    <mesh scale={scale} {...rest} ref={mesh}>
+      {renderShape[shape]}
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
 }
